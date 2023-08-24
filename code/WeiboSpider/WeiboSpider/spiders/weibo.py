682b011b15cb37e5acd5a9942d8ee5e6cfc7db16
FengGuoZhong <guozhong2000@126.com>
2023-08-24 11:28:42 +0800

    first submit python project code

---
+++ code/WeiboSpider/WeiboSpider/spiders/weibo.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class WeiboSpider(scrapy.Spider):
+    name = 'weibo'
+    allowed_domains = ['weibo.com']
+    start_urls = ['http://weibo.com/']
+
+    def parse(self, response):
+        pass
